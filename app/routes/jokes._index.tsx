a626aa873c82d36da62bd709bdf668f149980d9b
Priyank Thakkar <priyankthakkar@gmail.com>
2023-07-27 00:11:26 +1000

    feat: adding a random joke to display on index route for jokes

--- app/routes/jokes._index.tsx
+++ app/routes/jokes._index.tsx
@@ -1,8 +1,28 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    skip: randomRowNumber,
+    take: 1,
+  });
+
+  return json({
+    randomJoke,
+  });
+};
+
 export default function JokesIndexRoute() {
+  const { randomJoke } = useLoaderData<typeof loader>();
+
   return (
     <div>
       <p>A random Joke</p>
-      <p>I was wondering why the frisbee was getting bigger, then it him me!</p>
+      <p>{randomJoke.content}</p>
+      <Link to={randomJoke.id}>"{randomJoke.name}" Permalink</Link>
     </div>
   );
 }
