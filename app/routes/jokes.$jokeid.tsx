35d0a8c1bfcc8396d613350a6f1193079734c14e
Priyank Thakkar <priyankthakkar@gmail.com>
2023-07-26 23:06:59 +1000

    feat: Using individual joke route to display a joke based on joke id

--- app/routes/jokes.$jokeid.tsx
+++ app/routes/jokes.$jokeid.tsx
@@ -1,11 +1,31 @@
+import { LoaderArgs, json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeid },
+  });
+
+  if (!joke) {
+    throw new Error("Joke not found");
+  }
+
+  return json({
+    joke,
+  });
+};
+
 export default function JokeRoute() {
+
+  const { joke } = useLoaderData<typeof loader>();
+
   return (
     <div>
       <p>Here is your hilarious joke.</p>
-      <p>
-        Why don't we find hippopotamus hiding in the trees? Because they are
-        good at it.
+      <p>{joke.content}
       </p>
+      <Link to='.'>"{joke.name}" Permalink</Link>
     </div>
   );
 }
